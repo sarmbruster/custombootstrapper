38dd29936dd7cdd9a47571bc33e31db335edf2a0
Stefan Armbruster <stefan@armbruster-it.de>
2015-11-11 21:21:01 +0100

    override start() methods

--- src/main/java/org/neo4j/extension/CustomBootstrapper.java
+++ src/main/java/org/neo4j/extension/CustomBootstrapper.java
@@ -2,6 +2,7 @@ package org.neo4j.extension;
 
 import org.neo4j.server.NeoServer;
 import org.neo4j.server.enterprise.EnterpriseBootstrapper;
+import org.neo4j.server.enterprise.EnterpriseNeoServer;
 
 /**
  * @author Stefan Armbruster
@@ -11,6 +12,23 @@ public class CustomBootstrapper extends EnterpriseBootstrapper {
     @Override
     protected NeoServer createNeoServer() {
         System.out.println("loading custombootstrapper....");
-        return super.createNeoServer();
+
+        return new EnterpriseNeoServer(configurator, dependencies) {
+            @Override
+            public void start() {
+
+                // server's start: is called from bootstrapper's start
+                super.start();
+
+            }
+        };
+        //return super.createNeoServer();
+    }
+
+    @Override
+    public Integer start() {
+
+        // bootstraper's start is called first
+        return super.start();
     }
 }
